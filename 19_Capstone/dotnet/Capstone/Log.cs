42aca64ede45a87c57e255de0e565849d2ee9394
Scotty Henry <47279390+Scotty-Henry@users.noreply.github.com>
2019-07-14 22:54:27 -0400

    Add files via upload

---
+++ 19_Capstone/dotnet/Capstone/Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Capstone
+{
+    public class Log
+    {
+
+        public static void FeedMoneyLog(decimal moneyFed, decimal newBalance)
+        {
+            WriteLog("FEED MONEY", moneyFed, newBalance);
+        }
+
+        public static void PurchaseLog(string selection, string name, decimal oldBalance, decimal balance)
+        {
+            WriteLog(name, oldBalance, balance);
+        }
+
+        public static void ChangeLog(decimal oldBalance, decimal balance)
+        {
+            WriteLog("Give Change", oldBalance, balance);
+        }
+
+        private static void WriteLog(string message, decimal firstValue, decimal secondValue)
+        {
+            using (StreamWriter sw = new StreamWriter("transactionlog.txt", true))
+            {
+                sw.WriteLine($"{DateTime.Now.ToString().PadRight(21)}" +
+                             $"{message.PadRight(21)}" +
+                             $"{firstValue.ToString("C").PadRight(7)}" +
+                             $"{secondValue.ToString("C").PadRight(7)}");
+            }
+        }
+    }
+}
